ce506a667d401a922887f93af8910bb5e97cfd84
lantern <2050216062@qq.com>
2023-12-04 19:37:39 +0800

    Site updated: 2023-12-04 19:37:38

--- js/chatGPT.js
+++ js/chatGPT.js
@@ -19,3 +19,31 @@ function checkPassword2() {
         alert("密码错误！");
     }
 }
+
+function copyToClipboard1() {
+    // 获取要复制的内容
+    var content = document.getElementById("contentToCopy1").innerText;
+  
+    // 创建一个临时的 textarea 元素来选择文本
+    var tempElement = document.createElement("textarea");
+    tempElement.value = content;
+    document.body.appendChild(tempElement);
+    tempElement.select();
+    document.execCommand("copy"); // 执行复制操作
+    document.body.removeChild(tempElement); // 移除临时元素
+    alert("内容已复制到剪贴板"); // 可选：提供反馈
+}
+
+function copyToClipboard2() {
+    // 获取要复制的内容
+    var content = document.getElementById("contentToCopy2").innerText;
+  
+    // 创建一个临时的 textarea 元素来选择文本
+    var tempElement = document.createElement("textarea");
+    tempElement.value = content;
+    document.body.appendChild(tempElement);
+    tempElement.select();
+    document.execCommand("copy"); // 执行复制操作
+    document.body.removeChild(tempElement); // 移除临时元素
+    alert("内容已复制到剪贴板"); // 可选：提供反馈
+  }
