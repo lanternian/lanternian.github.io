8b0803e0c9d2c78c28a760662d98c2118a5c244e
lantern <2050216062@qq.com>
2023-12-10 15:20:50 +0800

    Site updated: 2023-12-10 15:20:50

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -81,6 +81,5 @@ NexT.utils.getAffixParam = function() {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-
   Affix.init(document.querySelector('.sidebar-inner'), NexT.utils.getAffixParam());
 });
